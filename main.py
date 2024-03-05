a6dd753bd2d76daf302048742cc6cfaa42a8ab07
CaptBarbarosa <tandogankaan5@gmail.com>
2024-03-05 18:26:00 +0000

    21.26

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import socket
 import scapy
 if __name__ == '__main__':
-    print('Hello from parrot terminal')
-    print('\n')
+    socket.setdefaulttimeout(2)
+    s=socket.socket()
+    s.connect(("192.168.95.148",21))
+    ans = s.recv(1024)
+    print(ans)
